386138ef4e7b9301a6a37ce93846f2b7736eb303
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-02-16 23:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-var randomNum;
-var input;
+//your code here
+var btn = document.getElementById("btn");
+var numPtag = document.getElementById("num");
+var res = document.getElementById("respond");
+var inputValue = document.getElementById("guess").value;
 
 function randomNumGenerator() {
-  randomNum = Math.floor(Math.random() * 41) - 20;
-  document.getElementById("num").innerText = randomNum;
-  guessNum();
-  print();
+	randomNum = Math.floor(Math.random() * 41) - 20;
+	document.getElementById("num").innerText = randomNum;
+	guessNum();
+	print();
 }
-
 function guessNum() {
-  input = Number(document.getElementById("guess").value);
+	input = Number(document.getElementById("guess").value);
 }
-function print() {
-  if (Math.abs(randomNum - input) <= 5) {
-    document.getElementById("respond").innerText = "Hot";
-  } else {
-    document.getElementById("respond").innerHTML = "Cold";
-  }
+function print () {
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerText = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+	
 }
-
+btn.addEventListener("click" randomNumGenerator);
 
