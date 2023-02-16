f1c5d2b30d261e9110cbb3d1a20b4549bb52d0e8
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-02-16 23:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+var btn = document,getElementById("btn");
+var numPtag = document.getElementById("num");
+var res = document.getElementById("respond");
+var inputValue = document.getElementById("guess").value;
+
+function randomNumberGenerator(){
+	randomNumber = Math.floar(Math.random() * 41) - 20;
+	document.getElementById("num").innerText = randomNumber;
+	guessNumber();
+	printValue();
+}
+function guessNumber (){
+	input = Number(document.getElementById("guess").value);
+}
+function printValue () {
+	if(Math.abs(randomNumber - input) <= 5){
+		document.getElementById("respond").innerText = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+	
+}
+btn.addEventListener("click" randomNumberGenerator);
 
