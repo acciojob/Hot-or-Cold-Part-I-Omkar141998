77337d99c96287d2e335e84eec6928106c19dba3
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-02-16 23:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-var btn = document,getElementById("btn");
+var btn = document.getElementById("btn");
 var numPtag = document.getElementById("num");
 var res = document.getElementById("respond");
 var inputValue = document.getElementById("guess").value;
